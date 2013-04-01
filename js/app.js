39148ac266f5bfdf27882edc3bb2f3d1565d4dbe
Kenshiro Nakagawa <kencheeto@gmail.com>
2013-03-31 17:34:59 -0700

    add facility and service models

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
+$(function() {
+  var Facility, Service;
 
+  Facility = Backbone.Model.extend({
+    defaults: function() {
+      return {
+        name:         'glide sf',
+        addr:         '123 street',
+        description:  'a place with services',
+        phone:        '(111)222-3333',
+        hours:        {},
+        notes:        'line up early!',
+        demographics: [],
+        geopoint:     {}
+      };
+    },
+
+  });
+
+  Service = Backbone.Model.extend({
+    defaults: function() {
+      return {
+        facilityId:   'glide sf', // reference to Facility
+        name:         'showers',  // like 'showers', 'hiv testing'
+        category:     'hygiene',  // 'hygiene'|'food'|'medical'|'shelter'|'technology'
+        hours:        {},
+        notes:        '3 showers',
+        demographics: []          // maybe?  unclear whether necessary 
+      };
+    }
+  });
+});
