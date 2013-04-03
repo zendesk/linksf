cf67f33d562f41c41d109ebfc7c4ed3dcf4e5eda
Ben Osheroff <ben@zendesk.com>
2013-04-02 20:29:09 -0700

    checkpoint

--- js/app.js
+++ js/app.js
@@ -9,13 +9,14 @@ $(function() {
     defaults: function() {
       return {
         name:         'glide sf',
-        addr:         '123 street',
+        address:      '123 street',
         description:  'a place with services',
         phone:        '(111)222-3333',
         hours:        {},
         notes:        'line up early!',
-        demographics: [],
-        geopoint:     {}
+        gender:       null,
+        age:         null,
+        location:     null
       };
     },
   });
@@ -23,12 +24,11 @@ $(function() {
   Service = Parse.Object.extend('Service', {
     defaults: function() {
       return {
-        facilityId:   'glide sf', // reference to Facility
         name:         'showers',  // like 'showers', 'hiv testing'
         category:     'hygiene',  // 'hygiene'|'food'|'medical'|'shelter'|'technology'
-        hours:        {},
-        notes:        '3 showers',
-        demographics: []          // maybe?  unclear whether necessary 
+        description:  null,
+        hours:        null,
+        notes:        null
       };
     }
   });
