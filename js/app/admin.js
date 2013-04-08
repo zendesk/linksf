eb9c45eb6f014f79feae60190fd7da20fe550853
Kenshiro Nakagawa <kencheeto@gmail.com>
2013-04-07 19:40:43 -0700

    rendering things now

--- js/app/admin.js
+++ js/app/admin.js
@@ -1,6 +1,16 @@
-var $ = require('jquery'),
-    Backbone = require('backbone');
+var $ = require('jquery');
 
-// var AdminView = Backbone.View.extend({
-//   el: $("#linksf-admin")
-// });
+$(function() {
+  var Backbone = require('backbone');
+
+  var AdminView = Backbone.View.extend({
+    el: $("#linksf-admin"),
+    render: function() {
+      $(this.el).html('hellooooo');
+      return this;
+    }
+  });
+
+  Admin = new AdminView();
+  Admin.render();
+});
