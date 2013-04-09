757cfe9f8d28a3b493f0e1e0da6dfd50931d30a7
Kenshiro Nakagawa <kencheeto@gmail.com>
2013-04-09 01:39:11 -0700

    bundle templates in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,41 +34,47 @@ module.exports = function(grunt) {
     // this will be the js src'd in <script> tags
     var output;
 
-    // require vendor js first
-    var output = shim(browserify(), {
+    // vendor js
+    output = shim(browserify(), {
       jquery: { path: './js/vendor/jquery.min.js', exports: '$' },
       'jquery-serialize-object': { path: './js/vendor/jquery.serialize-object.js', exports: '' }
     })
       .require('./js/shims/parse.js', {expose: 'parse'})
-      .require('./js/shims/google-maps.js', {expose: 'google-maps'})
-      .require('underscore', {expose: 'underscore'})
-      .require('backbone', {expose: 'backbone'});
+      .require('./js/shims/google-maps.js', {expose: 'google-maps'});
 
-    // and our application entry point
-    output = output.require('./js/app/index.js', {entry: true});
+    // use hbsfy transform to support requiring .hbs files
+    output = output.transform('hbsfy');
 
-
-    // require application modules
+    // application modules
+    //   note: this is optional as browserify traverses the AST from the entry point for require calls.
+    //   this gives us consistent require paths by exposing modules using a nicer name.
     [
+      './js/app/lib/*.js',
       './js/app/models/*.js',
-      './js/app/lib/*.js'
-
+      './js/app/views/*.js',
+      './js/app/templates/*.hbs'
     ].forEach(function(modules) {
       require('glob')(modules, function(er, files) {
         files.forEach(function(file) {
           var name = file
-            .replace("./js/app/","")
-            .replace(/\.js$/,"");
+            .replace('./js/app/','')
+            .replace(/\.(js|hbs)$/,'');
 
           output = output.require(file, {expose: name});
         });
       });
     });
 
+    // add entry point; browserify will traverse the AST tree from here for require calls
+    output = output.require('./js/app/admin.js', { entry: true });
+
     // now bundle it all up!
     output.bundle(function (err, src) {
       if (err) return console.error(err);
+
       require('fs').writeFileSync("./js/static/output.js", src);
+
+      // keep grunt alive until the write stream completes
       done();
     });
   });
