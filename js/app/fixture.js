8fcaecba11b575ee09fd26499e47698f787805c2
Ben Osheroff <ben@zendesk.com>
2013-04-08 21:07:11 -0700

    try making fixture.js a module

--- js/app/fixture.js
+++ js/app/fixture.js
@@ -56,33 +56,5 @@ $(function() {
     });
   };
 
-//  buildFacility({
-//    name:               "A Woman's Place",
-//    addr:               "1049 Howard St (between 6th and 7th)",
-//    addr_for_geocoding: "1049 Howard St, San Francisco, CA",
-//    description:        "A Place for Women",
-//    phone:              "(415) 487-2140",
-//    demographics:       ["W"],
-//    notes:              "",
-//    hours:              {},
-//    services: [
-//      {
-//        name:         "showers",
-//        category:     "hygiene",
-//        hours:        {"*": ["8am-4:30pm"]},
-//        description:  "",
-//        notes:        "Towels are sometimes provided but you may need to bring your own. Soap and Shampoo are provided.",
-//        demographics: ["W"]
-//      },
-//      {
-//        name:         "shelter",
-//        category:     "shelter",
-//        hours:        null, // means inherit hours of top level?
-//        description:  "5 beds for women in crisis (rape or domestic violence); stay up to 7 days. 16 shelter beds; stay varies. 16 beds in supportive housing (5 for HIV+ women); stay up to 18 months. 8-bed substance abuse program for any woman 18+; stay 1-4 months.",
-//        notes:        "Call for interview. Phone line open 24 hours a day.   Drop in 12pm-4pm for referrals & case management.",
-//        demographics: ["W"]
-//      }
-//  ]
-//  });
-//
+  module.exports = buildFacility;
 });
