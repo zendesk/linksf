952c1b1236325f6db533d6eac0be01fa9af34adb
Kenshiro Nakagawa <kencheeto@gmail.com>
2013-04-10 16:31:21 -0700

    remove logging

--- js/app/views/admin_view.js
+++ js/app/views/admin_view.js
@@ -5,9 +5,6 @@ var AdminView = Backbone.View.extend({
   el: $("#linksf"),
   template: require('templates/admin'),
   render: function() {
-    console.log('template is', this.template());
-    console.log('element is', $(this.el).html());
-    console.log('rendering admin!');
     $(this.el).html(this.template());
     return this;
   }
