cd6c7e0db4265776fa653570c401e2ff2efadc41
Shajith Chacko <shajith@zendesk.com>
2013-04-12 08:56:29 -0700

    added an hours model+tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,13 +17,30 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      files: ['Gruntfile.js', '<%= jshint.files %>', 'js/app/**/*.hbs', 'index.html'],
-      tasks: ['jshint', 'browserify']
+      files: ['Gruntfile.js',
+              '<%= jshint.files %>',
+              'js/app/**/*.hbs',
+              'test/**/*.js',
+              'index.html'],
+      tasks: ['jshint', 'simplemocha', 'browserify']
+    },
+    simplemocha: {
+      options: {
+        globals: ['should'],
+        timeout: 3000,
+        ignoreLeaks: false,
+        grep: '*-test',
+        ui: 'bdd',
+        reporter: 'tap'
+      },
+
+      all: { src: ['test/**/*.js'] }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-simple-mocha');
 
   grunt.registerTask('browserify', 'Browserify and concatenate app sources', function() {
 
@@ -80,5 +97,5 @@ module.exports = function(grunt) {
     });
   });
 
-  grunt.registerTask('default', ['jshint', 'browserify']);
+  grunt.registerTask('default', ['jshint', 'simplemocha', 'browserify']);
 };
