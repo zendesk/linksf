41a6645c0e6b38a4394e589b37bc6fb7512f2890
Kenshiro Nakagawa <kencheeto@gmail.com>
2013-04-13 01:18:44 -0700

    remove merge vestige

--- js/app/routers/router.js
+++ js/app/routers/router.js
@@ -1,11 +1,8 @@
 var Backbone = require('backbone'),
     AppView = require('views/app_view'),
     AdminView = require('views/admin_view'),
-<<<<<<< HEAD
-    DetailView = require('views/detail_view');
-=======
+    DetailView = require('views/detail_view'),
     ListView = require('views/list_view');
->>>>>>> 342628d9de2c8ef7b9f7550a0872bf1c92c7662a
 
 var Router = Backbone.Router.extend({
   routes: {
