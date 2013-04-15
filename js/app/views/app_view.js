45bd3dba6107ecc88cc255dc3fb2fdfa3ba5b429
Kenshiro Nakagawa <kencheeto@gmail.com>
2013-04-14 22:32:20 -0700

    extract querying into query object

--- js/app/views/app_view.js
+++ js/app/views/app_view.js
@@ -1,50 +1,17 @@
 var Backbone = require('backbone'),
     $ = require('jquery'),
-    _        = require('underscore'),
-    browse   = require('lib/browse'),
-    Modernizr = require('modernizr');
-
-function submitToParse(params) {
-  var browseFunction;
-
-  if ( params.runwhere === 'cloud' ) {
-    browseFunction = _.partial(Parse.Cloud.run, "browse");
-  } else {
-    browseFunction = browse;
-  }
-
-  browseFunction(params, {
-    success: function(result) {
-      $('#results').empty();
-      $('#results').append("results available at $('#results').data('results')\n\n");
-
-      _.each(result, function(fac) {
-        fac.set("services",
-          _.map(fac.get('services'), function(s) { return s.attributes; } )
-          );
-        $('#results').append(JSON.stringify(fac, null, '  '));
-        $('#results').append("\n");
-      });
-      $('#results').data('results', result);
-    }, error: function(err) {
-      $('#results').html(err);
-    }
-  });
-}
+    _ = require('underscore');
 
 var AppView = Backbone.View.extend({
   el: $("#linksf"),
   template: require('templates/query_form'),
+
   onSubmit: function() {
     var params = $('#query').serializeObject();
-    if ( params.sort == 'near' ) {
-      var location = require('lib/query').location(false);
-      $.extend(params, location);
-    }
-
-    submitToParse(params);
+    require('lib/query').submit(params);
     return false;
   },
+
   render: function() {
     $(this.el).html(this.template());
     $('#query').submit(this.onSubmit);
