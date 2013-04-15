9c028b66dac54f6818b7e15faba3ba85cd73f916
Kenshiro Nakagawa <kencheeto@gmail.com>
2013-04-14 22:32:20 -0700

    extract querying into query object

--- js/app/lib/query.js
+++ js/app/lib/query.js
@@ -1,3 +1,6 @@
+var $ = require('jquery'),
+    _ = require('underscore');
+
 var location = function(hasGeolocation) {
   var lat, lon;
 
@@ -17,8 +20,51 @@ var location = function(hasGeolocation) {
   return {lat: lat, lon: lon};
 };
 
+var dumpToDOM = function(result) {
+  var results = $('#results');
+  results.empty();
+  results.append("results available at $('#results').data('results')\n\n");
+
+  _.each(result, function(fac) {
+    fac.set("services",
+      _.map(fac.get('services'), function(s) { return s.attributes; } )
+      );
+    results.append(JSON.stringify(fac, null, '  '));
+    results.append("\n");
+  });
+  results.data('results', result);
+};
+
+var queryFunction = function(runInCloud) {
+  if ( runInCloud ) {
+    return _.partial(Parse.Cloud.run, "browse");
+  } else {
+    return require('lib/browse');
+  }
+};
+
+var submit = function(params) {
+  // choose where to run the query
+  var query = queryFunction(params.runwhere);
+
+  // add location if proximity sorting
+  if ( params.sort === 'near' ) {
+    // false until we start testing geolocation
+    $.extend(params, location(false));
+  }
+
+  query(params, {
+    success: function(result) {
+      dumpToDOM(result);
+    }, error: function(err) {
+      console.error(err);
+    }
+  });
+};
+
 var Query = {
-  location: location
+  location: location,
+  submit: submit
 };
 
 module.exports = Query;
