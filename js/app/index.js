2998e22096f9df9e73f2bc5ceea9cb2a1b02d0de
Kenshiro Nakagawa <kencheeto@gmail.com>
2013-04-14 22:48:08 -0700

    use router to visit root route, not view directly

--- js/app/index.js
+++ js/app/index.js
@@ -3,18 +3,17 @@ var $ = require('jquery');
 require('jquery-serialize-object');
 
 $(function() {
-  var AppView  = require('views/app_view'),
-      Router   = require('routers/router'),
-      appView;
+  var App = {},
+      Router = require('routers/router');
 
   Parse.initialize("Z2l0Zn6NGrHCDoBPKUeD7Tf1fAUDaazQihQFqnL8", "kGPp7cydleuFbhKB4mrviTmbIjrbTjhxGP4dP7Ls");
 
-  new Router();
+  // add to global namespace since we need to access router
+  App.Router = new Router();
 
-  appView = new AppView();
-  appView.render();
-  // execute default query
-  appView.onSubmit();
-
+  // begin tracking hashChange
   require('backbone').history.start();
+
+  // navigate to root route
+  App.Router.navigate('');
 });
