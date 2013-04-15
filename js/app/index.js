7c0fce1aa3ccba7d63697892abef4ae857f498f8
Kenshiro Nakagawa <kencheeto@gmail.com>
2013-04-14 20:53:37 -0700

    execute default query

--- js/app/index.js
+++ js/app/index.js
@@ -5,13 +5,16 @@ require('jquery-serialize-object');
 $(function() {
   var AppView  = require('views/app_view'),
       Router   = require('routers/router'),
-      appView, router;
+      appView;
 
   Parse.initialize("Z2l0Zn6NGrHCDoBPKUeD7Tf1fAUDaazQihQFqnL8", "kGPp7cydleuFbhKB4mrviTmbIjrbTjhxGP4dP7Ls");
 
   new Router();
 
+  appView = new AppView();
   appView.render();
+  // execute default query
+  appView.onSubmit();
 
   require('backbone').history.start();
 });
