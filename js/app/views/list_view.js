92e5bca560390d8402558c55046c9f0f426f9a4d
Kenshiro Nakagawa <kencheeto@gmail.com>
2013-04-16 23:54:18 -0700

    teach list view to submit query on form submission

--- js/app/views/list_view.js
+++ js/app/views/list_view.js
@@ -1,15 +1,35 @@
-/*globals App*/
-
 var Backbone = require('backbone'),
-    $ = require('jquery');
+    $ = require('jquery'),
+    Query = require('lib/query');
 
 var ListView = Backbone.View.extend({
   el: $("#linksf"),
   template: require('templates/list'),
 
+  submitQuery: function() {
+    // serialize the form
+    var params = $('#query').serializeObject(),
+        self = this;
+
+    // submit query
+
+    Query.submit(params).done(function(results) {
+      // populate with results
+      self.collection.reset(results.data);
+    });
+
+    // prevent default form submission
+    return false;
+  },
+
   render: function() {
     var facilities = this.collection.toJSON();
+
+    // replace with template
     $(this.el).html(this.template({ facilities: facilities }));
+
+    // bind to form submission
+    $('#query').submit($.proxy(this.submitQuery, this));
     return this;
   }
 });
