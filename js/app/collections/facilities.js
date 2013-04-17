202dbc56cacc703d39e44ca4ec704fe8a3362efd
Kenshiro Nakagawa <kencheeto@gmail.com>
2013-04-16 23:45:41 -0700

    make facilities collection a singleton

--- js/app/collections/facilities.js
+++ js/app/collections/facilities.js
@@ -2,15 +2,15 @@ var Parse       = require('parse');
 var _           = require('underscore');
 var Facility    = require('cloud/models/facility');
 
-module.exports = Parse.Collection.extend({
+var Facilities = Parse.Collection.extend({
   initialize: function(filter) {
     this.filter = filter;
   },
 
   add: function(models, options) {
     models = _.isArray(models) ? models.slice() : [models];
-    _.each(models, function(m) { 
-      if ( !m.matchesFilter || m.matchesFilter(this.filter) ) { 
+    _.each(models, function(m) {
+      if ( !m.matchesFilter || m.matchesFilter(this.filter) ) {
         Parse.Collection.prototype.add.call(this, [m], options);
       }
     }.bind(this));
@@ -18,4 +18,7 @@ module.exports = Parse.Collection.extend({
 
   more: function() {
   }
-}, {});
+});
+
+var instance = new Facilities();
+module.exports = { instance: instance };
