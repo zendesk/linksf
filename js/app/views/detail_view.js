f204c2efccdc54a6b98bc8b4a468ec8d418d6097
Ben Osheroff <ben@zendesk.com>
2013-05-10 11:40:00 -0700

    whitespace cleanup

--- js/app/views/detail_view.js
+++ js/app/views/detail_view.js
@@ -7,51 +7,48 @@ var DetailView = Backbone.View.extend({
   el: $("#linksf"),
   template: require('templates/detail'),
   events: {
-	"render.done": 'setMap'
+    "render.done": 'setMap'
   },
+
   render: function() {
-	var facility = this.model;
-	var $mapdiv =  this.$('#location-map');
+    var facility = this.model;
+    var $mapdiv =  this.$('#location-map');
 
     $(this.el).html(this.template({facility: facility}));
-	_.defer( function( view ){ view.setMap();}, this );
+    _.defer( function( view ){ view.setMap();}, this );
+
     return this;
   },
+
   setMap: function(){
-	if(this.$('#location-map')) {
-		var location = new gmaps.LatLng(
+    if(this.$('#location-map')) {
+      var location = new gmaps.LatLng(this.model.location.latitude,
+                                      this.model.location.longitude);
+      var mapOptions = {
+            center: location,
+            zoom: 15,
+            mapTypeId: gmaps.MapTypeId.ROADMAP,
+            scrollwheel: false,
+            streetViewControl: false,
+            zoomControlOptions: {
+              style: gmaps.ZoomControlStyle.SMALL
+            }
+          };
+      var map = new gmaps.Map(this.$('#location-map')[0], mapOptions);
 
-			this.model.location.latitude,
-			this.model.location.longitude
-		),
-		mapOptions = {
-			center: location,
-			zoom: 15,
-			mapTypeId: gmaps.MapTypeId.ROADMAP,
-			scrollwheel: false,
-			streetViewControl: false,
-			zoomControlOptions: {
-			style: gmaps.ZoomControlStyle.SMALL
-		}
-
-	},
-	map = new gmaps.Map(this.$('#location-map')[0], mapOptions);
-
-	new gmaps.Marker({
-		map: map,
-		position: location,
-		draggable: false
-		});
-	}
-	this.layout();
-	},
+      new gmaps.Marker({map: map,
+                        position: location,
+                        draggable: false});
+      }
+      this.layout();
+  },
   layout: function(){
-	$.each($('.desco'), function(){
-		if($(this).text().length > 52){
-			$(this).text($(this).text().substr(0,52)+"...");}
-		});
-	$('.more').click(function(){$(this).parent().next($('.seeMore')).slideToggle();});
-	}
+  $.each($('.desco'), function(){
+    if($(this).text().length > 52){
+      $(this).text($(this).text().substr(0,52)+"...");}
+    });
+  $('.more').click(function(){$(this).parent().next($('.seeMore')).slideToggle();});
+  }
 
 });
 
