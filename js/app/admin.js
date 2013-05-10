ae85e5191a57f335d5a60739a0bd5cb9d40c38db
Ben Osheroff <ben@zendesk.com>
2013-05-09 19:24:13 -0700

    admin skeleton

---
+++ js/app/admin.js
@@ -0,0 +1,21 @@
+var $ = require('jquery');
+
+require('jquery-serialize-object');
+
+$(function() {
+  Parse.initialize("Z2l0Zn6NGrHCDoBPKUeD7Tf1fAUDaazQihQFqnL8", "kGPp7cydleuFbhKB4mrviTmbIjrbTjhxGP4dP7Ls");
+
+  // provision the router instance
+  var router = require('routers/router').instance;
+
+  // begin tracking hashChange
+  require('backbone').history.start();
+
+  // use filter button to toggle form
+  $('#filter').click(function() {
+    $('#query').toggle();
+  });
+
+  // navigate to root route
+  // router.navigate('');
+});
