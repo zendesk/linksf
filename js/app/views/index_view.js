38acb4717d0fc579192a01763cd41117ffc9bc75
Igor Justino de Souza <igor@zendesk.com>
2013-05-21 16:53:01 -0700

    Link index categories buttons to #query route

--- js/app/views/index_view.js
+++ js/app/views/index_view.js
@@ -1,11 +1,21 @@
 var Backbone = require('backbone'),
     $        = require('jquery');
 
+function setup($el) {
+  $el.find('ul.categories li button').click(function() {
+    var route = 'query/' + $(this).data('category'),
+        router = require('routers/router').instance;
+
+    router.navigate(route, { trigger: true });
+  });
+}
+
 var IndexView = Backbone.View.extend({
   el: $("#linksf"),
   template: require('templates/index'),
   render: function() {
     this.$el.html(this.template());
+    setup(this.$el);
     return this;
   }
 });
