d20afe214cec41089a2ce808597ba33a87effc15
Igor Justino de Souza <igor@zendesk.com>
2013-05-21 17:11:04 -0700

    backbone like events for IndexView

--- js/app/views/index_view.js
+++ js/app/views/index_view.js
@@ -1,22 +1,24 @@
 var Backbone = require('backbone'),
     $        = require('jquery');
 
-function setup($el) {
-  $el.find('ul.categories li button').click(function() {
-    var route = 'query/' + $(this).data('category'),
-        router = require('routers/router').instance;
-
-    router.navigate(route, { trigger: true });
-  });
-}
-
 var IndexView = Backbone.View.extend({
   el: $("#linksf"),
   template: require('templates/index'),
+
+  events: {
+    'click ul.categories button': 'navigateToCategory'
+  },
+
   render: function() {
     this.$el.html(this.template());
-    setup(this.$el);
     return this;
+  },
+
+  navigateToCategory: function(event) {
+    var route  = 'query/' + $(event.target).data('category'),
+        router = require('routers/router').instance;
+
+    router.navigate(route, { trigger: true });
   }
 });
 
