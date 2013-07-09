aac0fc9889b85d40a5e38bc9f0fa23afa4106425
Ben Osheroff <ben@zendesk.com>
2013-07-09 16:32:59 -0700

    first decent pass at pagination/scroll

---
+++ js/app/lib/scroll.js
@@ -0,0 +1,43 @@
+/* globals document,window */
+var $ = require('jquery');
+
+var InfiniteScrollControl = function(onLoadFunc) {
+  this.onLoadMoreFunc = onLoadFunc;
+};
+
+$.extend(InfiniteScrollControl.prototype, {
+  isLoading: false,
+  isDisabled: false,
+   
+  _getDocumentHeight: function() {
+    return Math.max(
+        Math.max(document.body.scrollHeight, document.documentElement.scrollHeight),
+        Math.max(document.body.offsetHeight, document.documentElement.offsetHeight),
+        Math.max(document.body.clientHeight, document.documentElement.clientHeight)
+    );
+  },
+
+  triggerScroll: function() { 
+    if ( this.isLoading || this.isDisabled) {
+      return;
+    }
+
+    this.isLoading = true;
+    
+    if ( $(window).scrollTop() + $(window).height() >= this._getDocumentHeight() ) {
+      this.onLoadMoreFunc(this.onLoadMoreComplete.bind(this));
+    } else {
+      this.isLoading = false;
+      return;
+    }
+  },
+
+  onLoadMoreComplete: function(scrollAgain) { 
+    this.isLoading = false;
+    if ( !scrollAgain ) {
+      this.isDisabled = true;
+    }
+  }
+}); 
+
+module.exports = InfiniteScrollControl;
