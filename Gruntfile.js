7bbab5e2bfdf78ca51e913aceae8412572d2741f
Kenshiro Nakagawa <kencheeto@gmail.com>
2013-11-20 18:39:50 -0800

    add bootstrap-button dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -154,6 +154,7 @@ module.exports = function(grunt) {
       shared_js: [
         'vendor/js/jquery-2.0.3.js',
         'vendor/js/jquery.serialize-object.js',
+        'vendor/js/bootstrap-button.js',
         'vendor/js/underscore.js',
         'vendor/js/backbone-1.0.0.js',
         'vendor/js/parse-1.2.12.js'
@@ -162,6 +163,7 @@ module.exports = function(grunt) {
       shared_js_minified: [
         'vendor/js/jquery-2.0.3.min.js',
         'vendor/js/jquery.serialize-object.min.js',
+        'vendor/js/bootstrap-button.min.js',
         'vendor/js/underscore.min.js',
         'vendor/js/backbone-1.0.0.min.js',
         'vendor/js/parse-1.2.12.min.js'
@@ -223,7 +225,8 @@ module.exports = function(grunt) {
           // These libraries are not distributed with minified versions, so we
           // minify them ourselves.
           'vendor/js/jquery.serialize-object.min.js': 'vendor/js/jquery.serialize-object.js',
-          'vendor/js/backbone_filters.min.js': 'vendor/js/backbone_filters.js'
+          'vendor/js/backbone_filters.min.js': 'vendor/js/backbone_filters.js',
+          'vendor/js/bootstrap-button.min.js': 'vendor/js/bootstrap-button.js'
         }
       },
       app: {files: {'tmp/app.min.js': 'tmp/app.js'}},
