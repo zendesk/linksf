badfa607085eeea5a7b0e1630adc794a2278edfd
Kenshiro Nakagawa <kencheeto@gmail.com>
2013-11-25 09:11:17 -0800

    simplify computation in distance.js

--- shared/js/lib/distance.js
+++ shared/js/lib/distance.js
@@ -1,18 +1,18 @@
-var maps = google.maps,
+var computeDistanceBetween = google.maps.geometry.spherical.computeDistanceBetween,
     LatLng = maps.LatLng;
 
 function calculateDistanceFromService(location, currentLocation) {
   var pos1     = new LatLng(currentLocation.lat, currentLocation.lon),
       pos2     = new LatLng(location.latitude, location.longitude),
-      distance = maps.geometry.spherical.computeDistanceBetween(pos1, pos2);
-  distance = distance/1000*0.62137; // meters to miles
+      distance = computeDistanceBetween(pos1, pos2);
+  distance = distance/621.37; // meters to miles: distance / 1000 * 0.62137
   distance = +distance.toFixed(2); // precision after decimal point
   return distance;
 }
 
 function calculateWalkingTimeFromDistance(distance) {
   // 2.80 mph - older individuals average walking speed (wikipedia)
-  return Math.ceil(60*distance/2.5);
+  return Math.ceil(distance*24.0); // 60 * distance / 2.5
 }
 
 module.exports = {
