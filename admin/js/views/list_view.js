b16b2ceb8da55ba58d177ddf62b14834950ee821
Shajith Chacko <shajith@zendesk.com>
2013-12-12 09:03:08 -0800

    DRY-ed navigate function

--- admin/js/views/list_view.js
+++ admin/js/views/list_view.js
@@ -1,17 +1,5 @@
-var ListView = require('shared/views/list_view');
-
-function navigate(category) {
-  var route  = 'query',
-      params = [],
-      router = require('routers/router').instance();
-      if ( category.length > 0 ) {
-        params.push( "categories=" + category );
-      }
-      if (params.length > 0) {
-        route = route + "?" + params.join("&");
-      }
-      router.navigate(route, { trigger: true });
-}
+var ListView = require('shared/views/list_view'),
+    navigate = require('shared/lib/navigate');
 
 var AdminListView = ListView.extend({
   template: require('templates/admin_list'),
@@ -38,7 +26,7 @@ var AdminListView = ListView.extend({
   },
   filter: function(event) {
     var category = $(event.target).data('value');
-    navigate(category);
+    navigate({categories: [category]});
     return false;
   }
 });
