636f5b86dbf9157dc9b68cb67a3a35539fdd8eed
Kenshiro Nakagawa <kencheeto@gmail.com>
2014-01-15 15:55:19 -0800

    Revert "Revert "extract Google Analytics token and host. fixes #178""
    This reverts commit 0bcb8c643e18c8057c7c0c56500f255710dcb7b5.

--- tasks/configure.js
+++ tasks/configure.js
@@ -22,25 +22,36 @@ function ensureInEnv(variables) {
   });
 }
 
-function configureGlobalJson(grunt) {
-  var targetPath = 'server/config/global.json',
-      templatePath = targetPath + '.template',
+function configure(grunt, targetPath, data) {
+  var templatePath = targetPath + '.template',
       template = grunt.file.read(templatePath),
-      output,
-      env = process.env;
-
-  output = grunt.template.process(template, {
-    data: {
-      PARSE_DEV_APP_ID:      env.PARSE_DEV_APP_ID,
-      PARSE_DEV_MASTER_KEY:  env.PARSE_DEV_MASTER_KEY,
-      PARSE_PROD_APP_ID:     env.PARSE_PROD_APP_ID,
-      PARSE_PROD_MASTER_KEY: env.PARSE_PROD_MASTER_KEY
-    }
-  });
+      output = grunt.template.process(template, data);
 
   if ( !grunt.file.exists(targetPath) || grunt.file.read(targetPath) !== output ) {
     grunt.file.write(targetPath, output);
   }
+}
+
+function configureGoogleAnalytics(grunt) {
+  var targetPath = 'app/index.html',
+      data = {
+        GOOGLE_ANALYTICS_TOKEN: process.env.GOOGLE_ANALYTICS_TOKEN
+        GOOGLE_ANALYTICS_HOST: process.env.GOOGLE_ANALYTICS_HOST
+      };
+
+  configure(grunt, targetPath, data);
+}
+
+function configureGlobalJson(grunt) {
+  var targetPath = 'server/config/global.json',
+      data: {
+        PARSE_DEV_APP_ID:      process.env.PARSE_DEV_APP_ID,
+        PARSE_DEV_MASTER_KEY:  process.env.PARSE_DEV_MASTER_KEY,
+        PARSE_PROD_APP_ID:     process.env.PARSE_PROD_APP_ID,
+        PARSE_PROD_MASTER_KEY: process.env.PARSE_PROD_MASTER_KEY
+      };
+
+  configure(grunt, targetPath, data);
 }
 
 module.exports = function(grunt) {
@@ -50,13 +61,18 @@ module.exports = function(grunt) {
     ensureInEnv([
       'PARSE_DEV_APP_ID',
       'PARSE_DEV_JS_KEY',
-      'PARSE_DEV_MASTER_KEY'
+      'PARSE_DEV_MASTER_KEY',
+      'GOOGLE_ANALYTICS_DEV_TOKEN',
+      'GOOGLE_ANALYTICS_DEV_HOST'
     ]);
 
     process.env.PARSE_APP_ID = process.env.PARSE_DEV_APP_ID;
     process.env.PARSE_JS_KEY = process.env.PARSE_DEV_JS_KEY;
+    process.env.GOOGLE_ANALYTICS_TOKEN = process.env.GOOGLE_ANALYTICS_DEV_TOKEN;
+    process.env.GOOGLE_ANALYTICS_HOST = process.env.GOOGLE_ANALYTICS_DEV_HOST;
 
     configureGlobalJson(grunt);
+    configureGoogleAnalytics(grunt);
   });
 
   grunt.registerTask('configure:production', 'Configure for the production environment.', function() {
@@ -65,12 +81,17 @@ module.exports = function(grunt) {
     ensureInEnv([
       'PARSE_PROD_APP_ID',
       'PARSE_PROD_JS_KEY',
-      'PARSE_PROD_MASTER_KEY'
+      'PARSE_PROD_MASTER_KEY',
+      'GOOGLE_ANALYTICS_PROD_TOKEN',
+      'GOOGLE_ANALYTICS_PROD_HOST'
     ]);
 
     process.env.PARSE_APP_ID = process.env.PARSE_PROD_APP_ID;
     process.env.PARSE_JS_KEY = process.env.PARSE_PROD_JS_KEY;
+    process.env.GOOGLE_ANALYTICS_TOKEN = process.env.GOOGLE_ANALYTICS_PROD_TOKEN;
+    process.env.GOOGLE_ANALYTICS_HOST = process.env.GOOGLE_ANALYTICS_PROD_HOST;
 
     configureGlobalJson(grunt);
+    configureGoogleAnalytics(grunt);
   });
 };
