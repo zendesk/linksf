5832cad0cab3a0d9a4a7851582db114ea44250f5
Kenshiro Nakagawa <kencheeto@gmail.com>
2014-03-05 15:41:46 -0800

    have grunt ignore changes to minified vendored files
    uglify touches minified files every pass, so we don't want to trigger a rebuild in another watching grunt process - bitten by process collision during deploy.

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,6 +58,7 @@ module.exports = function(grunt) {
         'shared/**/*',
         'server/**/*',
         'vendor/**/*',
+        '!vendor/**/*.min.*',
         'test/**/*'
       ],
       tasks: ['default']
