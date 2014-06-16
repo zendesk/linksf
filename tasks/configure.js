80ad4ad1bc46f824ce6d86336599f79f8b50410a
Kenshiro Nakagawa <kencheeto@gmail.com>
2014-06-16 01:17:48 -0700

    more configure.js cleanup

--- tasks/configure.js
+++ tasks/configure.js
@@ -43,59 +43,78 @@ function configure(grunt, source, destination, context) {
   }
 }
 
-function configureAppTokens(grunt) {
-  configure(grunt, 'app/index.html', 'tmp/index.html', {
-    GOOGLE_ANALYTICS_TOKEN: process.env.GOOGLE_ANALYTICS_TOKEN,
-    GOOGLE_ANALYTICS_HOST: process.env.GOOGLE_ANALYTICS_HOST,
-    PARSE_APP_ID: process.env.PARSE_APP_ID,
-    PARSE_JS_KEY: process.env.PARSE_JS_KEY
-  });
+function configureApp(grunt) {
+  configure(grunt,
+    'app/index.html',
+    'tmp/index.html',
+    {
+      GOOGLE_ANALYTICS_TOKEN: process.env.GOOGLE_ANALYTICS_TOKEN,
+      GOOGLE_ANALYTICS_HOST:  process.env.GOOGLE_ANALYTICS_HOST,
+      PARSE_APP_ID:           process.env.PARSE_APP_ID,
+      PARSE_JS_KEY:           process.env.PARSE_JS_KEY
+    }
+  );
 }
 
-function configureAdminTokens(grunt) {
-  configure(grunt, 'admin/admin.html', 'tmp/admin.html', {
-    PARSE_APP_ID: process.env.PARSE_APP_ID,
-    PARSE_JS_KEY: process.env.PARSE_JS_KEY
-  });
+function configureAdmin(grunt) {
+  configure(grunt,
+    'admin/admin.html',
+    'tmp/admin.html',
+    {
+      PARSE_APP_ID: process.env.PARSE_APP_ID,
+      PARSE_JS_KEY: process.env.PARSE_JS_KEY
+    }
+  );
 }
 
-function configureMailgunTokens(grunt) {
-  configure(grunt, 'server/cloud/cloud/mailgun_credentials.js.template', 'server/cloud/cloud/mailgun_credentials.js', {
-    MAILGUN_DOMAIN: process.env.MAILGUN_DOMAIN,
-    MAILGUN_API_KEY: process.env.MAILGUN_API_KEY,
-    MAILGUN_TO_EMAIL_ADDRESS: process.env.MAILGUN_TO_EMAIL_ADDRESS,
-    MAILGUN_FROM_EMAIL_ADDRESS: process.env.MAILGUN_FROM_EMAIL_ADDRESS,
-  })
+function configureMailgun(grunt) {
+  configure(grunt,
+    'server/cloud/cloud/mailgun_credentials.js.template',
+    'server/cloud/cloud/mailgun_credentials.js',
+    {
+      MAILGUN_DOMAIN:             process.env.MAILGUN_DOMAIN,
+      MAILGUN_API_KEY:            process.env.MAILGUN_API_KEY,
+      MAILGUN_TO_EMAIL_ADDRESS:   process.env.MAILGUN_TO_EMAIL_ADDRESS,
+      MAILGUN_FROM_EMAIL_ADDRESS: process.env.MAILGUN_FROM_EMAIL_ADDRESS,
+    }
+  )
 }
 
-function configureGlobalJson(grunt) {
-  configure(grunt, 'server/config/global.json.template', 'server/config/global.json', {
-    PARSE_DEV_APP_ID:      process.env.PARSE_DEV_APP_ID,
-    PARSE_DEV_MASTER_KEY:  process.env.PARSE_DEV_MASTER_KEY,
-    PARSE_PROD_APP_ID:     process.env.PARSE_PROD_APP_ID,
-    PARSE_PROD_MASTER_KEY: process.env.PARSE_PROD_MASTER_KEY
-  });
+function configureParse(grunt) {
+  configure(grunt,
+    'server/config/global.json.template',
+    'server/config/global.json',
+    {
+      PARSE_DEV_APP_ID:      process.env.PARSE_DEV_APP_ID,
+      PARSE_DEV_MASTER_KEY:  process.env.PARSE_DEV_MASTER_KEY,
+      PARSE_PROD_APP_ID:     process.env.PARSE_PROD_APP_ID,
+      PARSE_PROD_MASTER_KEY: process.env.PARSE_PROD_MASTER_KEY
+    }
+  );
 }
 
 module.exports = function(grunt) {
-  grunt.registerTask('configure:development:app', 'Configure for the development app environment.', function() {
-    loadEnv(grunt, '.env');
+  grunt.registerTask('configure:development:app',
+    'Configure for the development app environment.',
+    function() {
+      loadEnv(grunt, '.env');
 
-    ensureInEnv([
-      'PARSE_DEV_APP_ID',
-      'PARSE_DEV_JS_KEY',
-      'PARSE_DEV_MASTER_KEY'
-    ]);
+      ensureInEnv([
+        'PARSE_DEV_APP_ID',
+        'PARSE_DEV_JS_KEY',
+        'PARSE_DEV_MASTER_KEY'
+      ]);
 
-    process.env.PARSE_APP_ID = process.env.PARSE_DEV_APP_ID;
-    process.env.PARSE_JS_KEY = process.env.PARSE_DEV_JS_KEY;
-    process.env.GOOGLE_ANALYTICS_TOKEN = process.env.GOOGLE_ANALYTICS_DEV_TOKEN;
-    process.env.GOOGLE_ANALYTICS_HOST = process.env.GOOGLE_ANALYTICS_DEV_HOST;
+      process.env.PARSE_APP_ID           = process.env.PARSE_DEV_APP_ID;
+      process.env.PARSE_JS_KEY           = process.env.PARSE_DEV_JS_KEY;
+      process.env.GOOGLE_ANALYTICS_TOKEN = process.env.GOOGLE_ANALYTICS_DEV_TOKEN;
+      process.env.GOOGLE_ANALYTICS_HOST  = process.env.GOOGLE_ANALYTICS_DEV_HOST;
 
-    configureGlobalJson(grunt);
-    configureAppTokens(grunt);
-    configureMailgunTokens(grunt);
-  });
+      configureParse(grunt);
+      configureApp(grunt);
+      configureMailgun(grunt);
+    }
+  );
 
   grunt.registerTask('configure:development:admin', 'Configure for the development admin environment.', function() {
     loadEnv(grunt, '.env');
@@ -109,33 +128,27 @@ module.exports = function(grunt) {
     process.env.PARSE_APP_ID = process.env.PARSE_DEV_APP_ID;
     process.env.PARSE_JS_KEY = process.env.PARSE_DEV_JS_KEY;
 
-    configureGlobalJson(grunt);
-    configureAdminTokens(grunt);
+    configureParse(grunt);
+    configureAdmin(grunt);
   });
 
   grunt.registerTask('configure:production', 'Configure for the production environment.', function() {
     loadEnv(grunt, '.env');
 
     ensureInEnv([
-      'MAILGUN_DOMAIN',
-      'MAILGUN_API_KEY',
-      'MAILGUN_TO_EMAIL_ADDRESS',
-      'MAILGUN_FROM_EMAIL_ADDRESS',
       'PARSE_PROD_APP_ID',
       'PARSE_PROD_JS_KEY',
-      'PARSE_PROD_MASTER_KEY',
-      'GOOGLE_ANALYTICS_PROD_TOKEN',
-      'GOOGLE_ANALYTICS_PROD_HOST'
+      'PARSE_PROD_MASTER_KEY'
     ]);
 
-    process.env.PARSE_APP_ID = process.env.PARSE_PROD_APP_ID;
-    process.env.PARSE_JS_KEY = process.env.PARSE_PROD_JS_KEY;
+    process.env.PARSE_APP_ID           = process.env.PARSE_PROD_APP_ID;
+    process.env.PARSE_JS_KEY           = process.env.PARSE_PROD_JS_KEY;
     process.env.GOOGLE_ANALYTICS_TOKEN = process.env.GOOGLE_ANALYTICS_PROD_TOKEN;
-    process.env.GOOGLE_ANALYTICS_HOST = process.env.GOOGLE_ANALYTICS_PROD_HOST;
+    process.env.GOOGLE_ANALYTICS_HOST  = process.env.GOOGLE_ANALYTICS_PROD_HOST;
 
-    configureGlobalJson(grunt);
-    configureAppTokens(grunt);
-    configureAdminTokens(grunt);
-    configureMailgunTokens(grunt);
+    configureParse(grunt);
+    configureApp(grunt);
+    configureAdmin(grunt);
+    configureMailgun(grunt);
   });
 };
