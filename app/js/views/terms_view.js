933b1616b2e77370c38c42d4c14c8c8c39c1bfef
Kenshiro Nakagawa <kencheeto@gmail.com>
2014-12-04 11:12:04 -0800

    Lay the base for the new terms & conditions page

---
+++ app/js/views/terms_view.js
@@ -0,0 +1,14 @@
+var TermsView = Backbone.View.extend({
+  options: {},
+  template: require('templates/terms'),
+  navButtons: [
+    { 'class': 'left', id: 'backNav-button', text: '<i class="icon-left-open back"></i> BACK' }
+  ],
+
+  render: function() {
+    this.$el.html(this.template());
+    return this;
+  }
+});
+
+module.exports = TermsView;
