6db7888dfaff2053666c05439edce1877828c1b0
Greg Ziegan <greg.ziegan@gmail.com>
2016-05-11 14:16:44 -0700

    open close logic

--- components/ServiceRow/ServiceRow.js
+++ components/ServiceRow/ServiceRow.js
@@ -1,6 +1,22 @@
 import React, { PropTypes } from 'react'
 import './ServiceRow.scss'
 
+const isOpen = (schedules) => {
+  const currentDay = 'Wednesday'
+  const currentTime = 1400
+
+  const isBetween = (num, min, max) => (
+    num > min && num < max
+  )
+
+  const isSomethingSchedule = schedule => (
+    currentDay === schedule.weekday &&
+    isBetween(currentTime, schedule.opensAt, schedule.closesAt)
+  )
+
+  return schedules.filter(isSomethingSchedule).length > 0
+}
+
 const ServiceRow = (props) => (
   <li className="service">
       <a href="/" className="serviceLink" title={`Click to see more details about ${props.name}`}>
@@ -9,7 +25,7 @@ const ServiceRow = (props) => (
             <p className="serviceName">{props.name}</p>
           </div>
             <span className="serviceInfo">
-              <span className="label-status open">open</span>
+              <span className="label-status something">{isOpen(props.regularSchedules) ? 'open' : 'closed'}</span>
             </span>
           <div className="categoryIcons">
               <span><i className="category-icon icon-home"></i></span>
