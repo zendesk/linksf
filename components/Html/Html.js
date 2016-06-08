8c23efe2f5f196dffcca0eda81b7f2f088259e14
Greg Ziegan <greg.ziegan@gmail.com>
2016-06-08 15:24:37 -0700

    added a google maps thingy 💎

--- components/Html/Html.js
+++ components/Html/Html.js
@@ -19,6 +19,7 @@ function Html({ title, description, body, debug }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto" />
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDDdHRnaDD-_z2aNUqyxxCoRBAf6PWZjS4&sensor=true&libraries=geometry" />
         <script src={'/app.js?' + new Date().getTime()} />
       </head>
       <body>
--- components/Service/Service.js
+++ components/Service/Service.js
@@ -1,16 +1,27 @@
 import React, { PropTypes } from 'react'
 import './Service.scss'
 
+import GoogleMap from '../GoogleMap'
+
 const Service = (props) => {
   const { service } = props
     return (
-      <div>
+      <div className="service">
+        <h2 className="title">Welcome</h2>
         <div className="inset">
-          {service.name}
+          Womens
         </div>
-        {service.description}
+        <h2 className="title">Services</h2>
+        <div className="inset">
+         Shelter
+        </div>
+        <div className="inset-map">
+          <GoogleMap />
+          <p>1049 Howard St</p>
+        </div>
+        
       </div>
     )
-  }
+}
 
 export default Service
