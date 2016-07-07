dfbb9a41fbefb43fee6ba3f5fa0f2d5e81a0a075
Jacob Pandl <jpandl19@gmail.com>
2016-07-06 18:48:11 -0700

    Linked up categories to firebase api. Created methods for fetching categories and organizations/ org services

--- components/CategoryList/CategoryList.js
+++ components/CategoryList/CategoryList.js
@@ -1,23 +1,28 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import s from './CategoryList.css'
 import Category from '../Category'
 
-const categories = [
-  { icon: 'icon-home', name: 'Shelter' },
-  { icon: 'icon-food', name: 'Food' },
-  { icon: 'icon-plus', name: 'Medical' },
-  { icon: 'icon-droplet', name: 'Hygiene' },
-  { icon: 'icon-desktop', name: 'Technology' },
-]
+// const categories = [
+//   { icon: 'icon-home', name: 'Shelter' },
+//   { icon: 'icon-food', name: 'Food' },
+//   { icon: 'icon-plus', name: 'Medical' },
+//   { icon: 'icon-droplet', name: 'Hygiene' },
+//   { icon: 'icon-desktop', name: 'Technology' },
+// ]
 
-const CategoryList = () => (
+const CategoryList = (props) => (
   <ul title="Services" className={`${s.categories} btn-group`}>
-    {categories.map((category, index) => (
-      <li className={s.categoryItem}>
+    {props.categories.map((category, index) => (
+      <li key={index} className={s.categoryItem}>
         <Category key={`category-${index}`} iconClass={category.icon} name={category.name} />
       </li>
     ))}
   </ul>
 )
+
+CategoryList.propTypes = {
+  categories: PropTypes.array.isRequired,
+}
+
 
 export default CategoryList
--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react'
 import s from './Home.css'
 import CategoryList from '../CategoryList'
+import {fetchCategories} from '../../core/firebase-api'
 
-const Home = class Home extends Component {
+const getCategories = (categoryNames) => (
+  categoryNames.map(name => (
+    { icon: 'icon-home', name: name[0].toUpperCase() + name.slice(1) }
+  ))
+)
+
+export default class extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      categories: [],
+      loading: true,
+    }
+  }
+
+  componentWillMount() {
+    fetchCategories().then((categories) => {
+      this.setState({
+        categories: categories,
+        loading: false,
+      })
+      console.log(this.state)
+    })
+  }
+
   render() {
+    const { categories } = this.state
     return (
       <div>
         <h1 className={s.title}>What service are you looking for?</h1>
         <form id="search-form">
-          <CategoryList />
+          <CategoryList categories={getCategories(categories)} />
         </form>
       </div>
     )
   }
 }
-
-export default Home
