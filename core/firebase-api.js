fc701bcd89b082db810b10afa9770f5ab9c17ffd
Jacob Pandl <jpandl19@gmail.com>
2016-07-13 12:15:34 -0700

    Fixed formatting for eslint

--- core/firebase-api.js
+++ core/firebase-api.js
@@ -5,29 +5,36 @@ const ref = new Firebase('https://vivid-inferno-4672.firebaseio.com')
 // Fetches an organization with its services. Returns a Promise
 export function fetchOrganization(id) {
   let organizationOut
-  return ref.child(`organizations/${id}`).once('value').then((orgRes)=> {
+  return ref.child(`organizations/${id}`).once('value').then((orgRes) => {
     //  Return the actual service object rather than the ugly firebase object
     organizationOut = orgRes.val()
     return organizationOut
-  }).then((organization)=>{
+  })
+  .then((organization) => {
     // use the service to fetch an organization
     return ref.child('services').orderByChild('organization').equalTo(organization.id).once('value')
-  }).then((services)=>{
+  })
+  .then((services) => {
     // Append the organization to the service
     const servicesObj = services.val()
     organizationOut.services = Object.keys(servicesObj).map(key => servicesObj[key])
-    return ref.child('regular_schedules').orderByChild('organization_id').equalTo(organizationOut.id).once('value')
-  }).then((hours)=>{
-  	// TODO: Format the hours so that they are part of their respective 'service' objects
+    return ref.child('regular_schedules').orderByChild('organization_id')
+                                         .equalTo(organizationOut.id)
+                                         .once('value')
+  })
+  .then((hours) => {
+  // TODO: Format the hours so that they are part of their respective 'service' objects
     const hoursObj = hours.val()
     organizationOut.service_hours = Object.keys(hoursObj).map(key => hoursObj[key])
     return ref.child(`phones/${id}`).once('value')
-  }).then((phones)=>{
-  	// Add array of org's phone #'s
+  })
+  .then((phones) => {
+  // Add array of org's phone #'s
     organizationOut.phones = phones.val()
     return ref.child(`locations/${id}`).once('value')
-  }).then((location)=>{
-  	// Add array of org's location info
+  })
+  .then((location) => {
+  // Add array of org's location info
     organizationOut.location = location.val()
     return organizationOut
   })
