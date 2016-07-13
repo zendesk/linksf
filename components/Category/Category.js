550f733cc13a7fd4c442b1682eb2c6d2d6ff0ea1
Adam Hart <ahart@zendesk.com>
2016-07-13 14:35:29 -0700

    Icons wurking

--- components/Category/Category.js
+++ components/Category/Category.js
@@ -6,7 +6,7 @@ import icons from '../../icons/css/icons.css'
 const category = (props) => (
   <Link className="mdl-navigation__link" to="/services">
     <button role="link" className={s.btn} aria-label="Look for ${props.name} services">
-      <i className={`icon-right-open-2 category-icon ${props.iconClass}`}></i>{props.name}
+      <i className={icons.iconRightOpen2 + ` category-icon ${props.iconClass}`}></i>{props.name}
     </button>
   </Link>
 )
