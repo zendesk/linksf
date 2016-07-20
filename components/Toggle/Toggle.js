5f04cbc1920fcbad917c6b78a4b42961c122d442
Greg Ziegan <greg.ziegan@gmail.com>
2016-07-20 14:13:44 -0700

    🚧 Toggle Work 🚧 (#287)
    * make it look purty 💅
    
    * toggles work w/o animation; fix nav bar styles
    
    * remove assets.json

--- components/Toggle/Toggle.js
+++ components/Toggle/Toggle.js
@@ -1,10 +1,28 @@
-import React, { Component } from 'react'
-import './Toggle.css'
+import React from 'react'
+import s from './Toggle.css'
 
-export default class extends Component {
-  render() {
-    return (
-      <input type="checkbox" value={this.props.value || 'X'}/>
-    )
-  }
-}
+
+const Toggle = (props) => (
+  <div
+    className={`${s.switch} ${props.disabled ? s.disabled : ''}`}
+    tabIndex="0"
+    onMouseUp={(e) => props.onMouseUp && props.onMouseUp(e)}
+  >
+    <div className={s.mask}>
+      <div className={`${s.container} ${props.on ? s.containerOn : s.containerOff}`}>
+        <div className={s.switchUpper}>
+          <span className={s.handle}></span>
+        </div>
+        <div className={s.switchLower}>
+          <div className={s.labels}>
+            <a href="#" className={s.on} tabIndex="-1">{props.onLabel || 'YES'}</a>
+            <a href="#" className={s.off} tabIndex="-1">{props.offLabel || 'NO'}</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className={s.switchMiddle}></div>
+  </div>
+)
+
+export default Toggle
