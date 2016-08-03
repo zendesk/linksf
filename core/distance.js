4e52f01433b69cdad0703dbdb994c1b0a5e30f8a
Greg Ziegan <greg.ziegan@gmail.com>
2016-08-03 13:39:09 -0700

    wip trying to promisify

--- core/distance.js
+++ core/distance.js
@@ -1,33 +1,53 @@
+import Promise from 'bluebird'
+
+function noErrPromisifier(originalMethod) {
+  return () => {
+    const args = [].slice.call(arguments)
+    const self = this
+    return new Promise((resolve) => {
+      args.push(resolve)
+      console.log(originalMethod)
+      originalMethod.apply(self, args)
+    })
+  }
+}
+
 function fetchDistanceAndDuration(origins, destinations) {
   const googleService = new google.maps.DistanceMatrixService()
-  return googleService.getDistanceMatrix(
-    {
-      origins,
-      destinations,
-      travelMode: google.maps.TravelMode.WALKING,
-      durationInTraffic: true,
-      avoidHighways: true,
-      avoidTolls: false,
-      unitSystem: google.maps.UnitSystem.IMPERIAL,
-    })
+  const promiseGoogleService = Promise.promisifyAll(googleService.getDistanceMatrix, { promisifier: noErrPromisifier })
+  return promiseGoogleService({
+    origins,
+    destinations: destinations.slice(0, 10),
+    travelMode: google.maps.TravelMode.WALKING,
+    durationInTraffic: true,
+    avoidHighways: true,
+    avoidTolls: false,
+    unitSystem: google.maps.UnitSystem.IMPERIAL,
+  })
 }
 
 function getResponseData(response, status) {
   const responseData = (status === 'OK') ? response.rows[0].elements : {}
-  console.log(responseData)
+  // console.log(response)
+  // console.log(responseData)
   return responseData
 }
 
 export function calculateDistance(location, currentLocation) {
   const pos = new google.maps.LatLng(location.latitude, location.longitude)
-  const googleLocation = new google.maps.LatLng(currentLocation.lat, currentLocation.lon)
+  const { latitude, longitude } = currentLocation.coords
+  const googleLocation = new google.maps.LatLng(latitude, longitude)
+  console.log('asdkjlfnlakjsdf')
+  console.log(latitude)
+  console.log(longitude)
 
   return fetchDistanceAndDuration([googleLocation], [pos])
-    .then(getResponseData)
 }
 
 export function calculateAllDistances(locations, currentLocation) {
-  const origins = [new google.maps.LatLng(currentLocation.lat, currentLocation.lon)]
+  const { latitude, longitude } = currentLocation.coords
+  const googleLocation = new google.maps.LatLng(latitude, longitude)
+  const origins = []
 
   const destinations = locations.map(location => (
     new google.maps.LatLng(
@@ -36,5 +56,4 @@ export function calculateAllDistances(locations, currentLocation) {
     )))
 
   return fetchDistanceAndDuration(origins, destinations)
-    .then(getResponseData)
 }
