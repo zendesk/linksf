5aa3352312ee104b7081ec61dec8357d1b2875ec
Adam Hart <ahart@zendesk.com>
2016-08-10 13:09:42 -0700

    Beginning of admin page

---
+++ pages/admin/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import Layout from '../../components/Layout'
+import Admin from '../../components/Admin'
+import Login from '../../components/Login'
+import icons from '../../icons/css/icons.css'
+import { fetchCategories } from '../../core/firebaseApi'
+
+class AdminPage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loggedIn: false,
+      username: "",
+      password: "",
+    }
+  }
+
+  componentDidMount() {
+    document.title = 'Admin'
+  }
+
+  render() {
+    const { loggedIn, username, password } = this.state
+    return (
+      <Layout>
+        {loggedIn ?
+          <Admin/> :
+          <Login/>
+        }
+      </Layout>
+    )
+  }
+}
+
+export default AdminPage
