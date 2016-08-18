d010c1062380177236c408b3e9bbc365c98dde50
Michael Huston <mhuston@zendesk.com>
2016-08-17 17:10:24 -0700

    Remove mailgun code due to cors limitations when running code in browser

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -8,36 +8,25 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React from 'react';
-import Layout from '../../components/Layout';
-var mailgun = require('mailgun.js');
+import React from 'react'
+import Layout from '../../components/Layout'
 
 class FeedbackPage extends React.Component {
 
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       name: '',
       email: '',
       subject: '',
-      message: ''
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
+      message: '',
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleSubmit(e) {
-    //  TODO: send email
-    console.log(this.state);
-    var mg = mailgun.client({username: 'api', key: process.env.MAILGUN_API_KEY || 'key-yourkeyhere'});
-
-    mg.messages.create('link-sf.com', {
-      from: "mhuston@zendesk.com",
-      to: "mhuston@zendesk.com",
-      subject: this.state.subject,
-      text: this.state.message
-    })
-    .then(msg => console.log(msg)) // logs response data
-    .catch(err => console.log(err)); // logs any error
+    e.preventDefault()
+    console.log(this.state)
   }
 
   render() {
@@ -48,32 +37,32 @@ class FeedbackPage extends React.Component {
             type="text"
             placeholder="Your name"
             value={this.state.name}
-            onChange={e => this.setState({name: e.target.value}) }
+            onChange={e => this.setState({ name: e.target.value })}
           />
           <input
             type="text"
             placeholder="Your email"
             value={this.state.email}
-            onChange={e => this.setState({email: e.target.value}) }
+            onChange={e => this.setState({ email: e.target.value })}
           />
           <input
             type="text"
             placeholder="Subject"
             value={this.state.subject}
-            onChange={e => this.setState({subject: e.target.value}) }
+            onChange={e => this.setState({ subject: e.target.value })}
           />
           <textarea
             type="text"
             placeholder="Message"
             value={this.state.message}
-            onChange={e => this.setState({message: e.target.value}) }
+            onChange={e => this.setState({ message: e.target.value })}
           />
           <input type="submit" value="Post" />
         </form>
       </Layout>
-    );
+    )
   }
 
 }
 
-export default FeedbackPage;
+export default FeedbackPage
