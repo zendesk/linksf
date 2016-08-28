e8b37ce8796dccd5949b5ec3e6ee6f880fc17fa3
Adam Hart <ahart@zendesk.com>
2016-08-28 16:21:44 -0700

    Cleanup merge a bit

--- components/AdminLocationRow/AdminLocationRow.js
+++ components/AdminLocationRow/AdminLocationRow.js
@@ -6,27 +6,27 @@ import ServiceStatus from '../ServiceStatus'
 
 const AdminLocationRow = (props) => (
   <li className={s.location} key={`location-${props.location.id}`}>
-      <a onClick={() => props.editLink(props.location)} className={s.locationLink} title={`Click to edit ${props.location.name}`}>
+    <a onClick={() => props.editLink(props.location)} className={s.locationLink} title={`Click to edit ${props.location.name}`}>
+      <div>
         <div>
-          <div>
-            <p className={s.locationName}>{props.location.name}</p>
+          <p className={s.locationName}>{props.location.name}</p>
+        </div>
+        <div className={s.locationBox}>
+          <div className={s.locationInfo}>
+            <span className={s.locationAddress}>
+              {`${props.location.physical_address.address_1}, ${props.location.physical_address.city}`}
+            </span>
+            <div className={s.categoryIcons}>
+              <span><i className={'category-icon ' + icons.iconHome}></i></span>
+            </div>
           </div>
-          <div className={s.locationBox}>
-            <div className={s.locationInfo}>
-              <span className={s.locationAddress}>
-                {`${props.location.physical_address.address_1}, ${props.location.physical_address.city}`}
-              </span>
-              <div className={s.categoryIcons}>
-                <span><i className={'category-icon ' + icons.iconHome}></i></span>
-              </div>
-            </div>
-            <div className="location-item-box-caret">
-              <i className={`${icons.iconRightOpen2} s.rightCaret`}></i>
-            </div>
+          <div className="location-item-box-caret">
+            <i className={`${icons.iconRightOpen2} s.rightCaret`}></i>
           </div>
         </div>
-      </a>
-    </li>
+      </div>
+    </a>
+  </li>
 )
 
 export default AdminLocationRow
