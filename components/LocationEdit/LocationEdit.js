20a7c475b3df757df1e412e28df3617d8b322655
Adam Hart <ahart@zendesk.com>
2016-09-07 17:45:41 -0700

    Refactor with orgs as top level

--- components/LocationEdit/LocationEdit.js
+++ components/LocationEdit/LocationEdit.js
@@ -4,95 +4,80 @@ import icons from '../../icons/css/icons.css'
 
 import ServiceEdit from '../ServiceEdit'
 
-class LocationEdit extends React.Component{
-  constructor(props) {
-    super(props)
-    this.state = {
-      location: props.location
-    }
-  }
+const LocationEdit = (props) => {
 
-  handleInput = (field, event) => {
-    const { location } = this.state
+  function updateLocation(field, value) {
+    const { location } = props
     const newLocation = location
-    newLocation[field] = event.target.value
-    this.setState(Object.assign(this.state, { location: newLocation }))
+    newLocation[field] = value
+    props.handleChange(newLocation, props.index)
   }
 
-  handleAddress = (field, event) => {
-    const { physical_address } = this.state.location
-    const new_physical_address = physical_address
-    new_physical_address[field] = event.target.value
-    this.setState(Object.assign(this.state, { physical_address: new_physical_address }))
+  function handleLocationChange(field, event) {
+    updateLocation(field, event.target.value)
   }
 
-  handleService = (new_service, index) => {
-    const { services } = this.state.location
+  function handleAddress(field, event) {
+    const { physical_address } = props.location
+    const newPhysicalAddress = physical_address
+    newPhysicalAddress[field] = event.target.value
+    updateLocation('physical_address', newPhysicalAddress)
+  }
+
+  function handleService(newService, index) {
+    const { services } = props.location
     const newServices = services
-    newServices[index] = new_service
-    this.setState(Object.assign(this.state, { services: newServices }))
+    newServices[index] = newService
+    updateLocation('services', newServices)
   }
 
-  handleSubmit = (event)  => {
-    // Do some stuff with firebase to login yee
-  }
-
-  render() {
-    return (
-      <div className={s.locationEdit}>
-        <div className={s.locationFields}>
-          <h4 className={s.sectionLabel}>Location</h4>
-          <div className={s.group}>
-            <span className={s.nameLabel}>Location Name </span>
-            <input
-              className={s.input}
-              type="text"
-              value={this.state.location.name}
-              onChange={(e) => this.handleInput('name', e)}
-            />
-          </div>
-          <div className={s.group}>
-            <span className={s.descriptionLabel}>Location Description </span>
-            <input
-              className={s.input}
-              type="text"
-              value={this.state.location.description}
-              onChange={(e) => this.handleInput('description', e)}
-            />
-          </div>
-          <div className={s.group}>
-            <span className={s.addressLineOneLabel}>Street Address </span>
-            <input
-              className={s.input}
-              type="text"
-              value={this.state.location.physical_address.address_1}
-              onChange={(e) => this.handleAddress('address_1', e)}
-            />
-          </div>
-          <div className={s.group}>
-            <span className={s.addressCityLabel}>City </span>
-            <input
-              className={s.input}
-              type="text"
-              value={this.state.location.physical_address.city}
-              onChange={(e) => this.handleAddress('city', e)}
-            />
-          </div>
+  return (
+    <div className={s.locationEdit}>
+      <div className={s.locationFields}>
+        <h4 className={s.sectionLabel}>Location</h4>
+        <div className={s.group}>
+          <span className={s.nameLabel}>Location Name </span>
+          <input
+            className={s.input}
+            type="text"
+            value={props.location.name}
+            onChange={(e) => handleLocationChange('name', e)}
+          />
         </div>
-        <div className={s.servicesBox}>
-          <h4 className={s.sectionLabel}>Services</h4>
-          {(this.state.location.services || []).map((service, index) => <ServiceEdit service={service} index={index} handleChange={this.handleService} /> )}
+        <div className={s.group}>
+          <span className={s.descriptionLabel}>Location Description </span>
+          <input
+            className={s.input}
+            type="text"
+            value={props.location.description}
+            onChange={(e) => handleLocationChange('description', e)}
+          />
         </div>
-        <div className={s.submit}>
-          <button
-            className={s.submitBtn}
-            type="submit"
-            onClick={this.handleSubmit}>Submit
-          </button>
+        <div className={s.group}>
+          <span className={s.addressLineOneLabel}>Street Address </span>
+          <input
+            className={s.input}
+            type="text"
+            value={props.location.physical_address.address_1}
+            onChange={(e) => handleAddress('address_1', e)}
+          />
+        </div>
+        <div className={s.group}>
+          <span className={s.addressCityLabel}>City </span>
+          <input
+            className={s.input}
+            type="text"
+            value={props.location.physical_address.city}
+            onChange={(e) => handleAddress('city', e)}
+          />
         </div>
       </div>
-    )
-  }
+      <div className={s.servicesBox}>
+        <h4 className={s.sectionLabel}>Services</h4>
+        {(props.location.services || []).map((service, index) => <ServiceEdit service={service} index={index} handleChange={handleService} /> )}
+      </div>
+    </div>
+  )
 }
 
 export default LocationEdit
