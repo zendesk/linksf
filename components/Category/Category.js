29143a1cfc0791648325668212e00d25dd39209f
Greg Ziegan <greg.ziegan@gmail.com>
2016-09-21 15:09:20 -0500

    Routing from Home page with Categories (#308)
    * wip commit
    
    * rebase?
    
    * filtering by category works
    
    * added index.html to the git ignore

--- components/Category/Category.js
+++ components/Category/Category.js
@@ -4,7 +4,7 @@ import Link from '../Link'
 import icons from '../../icons/css/icons.css'
 
 const category = (props) => (
-  <Link to="/locations">
+  <Link to="/locations" query={{ categories: [props.id] }}>
     <button role="link" className={s.btn} aria-label="Look for ${props.name} services">
       <i className={`${s.categoryIcon} ${props.iconClass}`}></i>{props.name}
       <i className={`${s.rightCaret} ${icons.iconRightOpen2}`}></i>
@@ -13,6 +13,7 @@ const category = (props) => (
 )
 
 category.propTypes = {
+  id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   iconClass: PropTypes.string,
 }
