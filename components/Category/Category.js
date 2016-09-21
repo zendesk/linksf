96dacd32c64f4493947c4e9ff959f5998de605ee
Greg Ziegan <greg.ziegan@gmail.com>
2016-09-21 10:33:02 -0700

    rebase?

--- components/Category/Category.js
+++ components/Category/Category.js
@@ -4,7 +4,7 @@ import Link from '../Link'
 import icons from '../../icons/css/icons.css'
 
 const category = (props) => (
-  <Link to="/locations">
+  <Link to="/locations" query={{ categories: ['housing', 'food'], demographics: 'Y' }}>
     <button role="link" className={s.btn} aria-label="Look for ${props.name} services">
       <i className={`${s.categoryIcon} ${props.iconClass}`}></i>{props.name}
       <i className={`${s.rightCaret} ${icons.iconRightOpen2}`}></i>
