ee4112b2d8053858365c56d80a0dfeff9f11eb12
Adam Hart <ahart@zendesk.com>
2016-10-12 13:26:04 -0700

    Add toggle button

---
+++ components/ToggleButton/ToggleButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import s from './ToggleButton.css'
+
+const ToggleButton = (props) => (
+  <button
+    className={`${s.toggleButton} ${props.enabled ? s.enabled : ''}`}
+    onClick={props.onClick}
+    title={props.label}
+  >
+  {props.label}
+  </button>
+)
+
+export default ToggleButton
