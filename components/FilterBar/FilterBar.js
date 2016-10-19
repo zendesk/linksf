e5c66eb335f4bc0cb4266e36a6a5cef1ae12dd86
Craig Day <cday@zendesk.com>
2016-10-19 16:15:19 -0700

    no more sort by name and move filter button

--- components/FilterBar/FilterBar.js
+++ components/FilterBar/FilterBar.js
@@ -1,18 +1,11 @@
 import React, { PropTypes } from 'react'
 import ToggleButton from '../ToggleButton'
-import Button from '../Button'
+
 import s from './FilterBar.css'
 import Link from '../Link'
 
 const FilterBar = (props) => (
   <div className={s.row}>
-    <div className={s.filterOption}>
-      <ToggleButton
-        label="Sort by name"
-        enabled={false}
-        on
-      />
-    </div>
     <div className={s.filterOption}>
       <ToggleButton
         label="Sort by distance"
@@ -27,9 +20,6 @@ const FilterBar = (props) => (
         onClick={props.onToggleOpen}
       />
     </div>
-    <Link to="/options">
-      <Button>OPTIONS</Button>
-    </Link>
   </div>
 )
 
