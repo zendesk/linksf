8ddfd38afbe84070be36bb734680de16acc7ad5d
Shakti Rajpurohit <srajpurohit@zendesk.com>
2016-11-09 15:57:51 -0800

    Shakti design v2 (#336)
    * open now and sort buttons
    
    * Got gender working
    
    Need to set current text
    
    * Stylez
    
    * Updated service and suitable for
    
    Added checkbox on select and updated design
    
    * Updated search button on top of options page
    
    * hiding alert if permission granted
    
    * Made updates based on Criag's comment
    
    * firebase deployz and scale everything down by 2
    
    * Add authentication to writes
    
    * open now and sort buttons
    
    * Got gender working
    
    Need to set current text
    
    * Updated service and suitable for
    
    Added checkbox on select and updated design
    
    * Updated search button on top of options page
    
    * hiding alert if permission granted
    
    * Made updates based on Criag's comment
    
    * reducing everything to half

--- pages/options/index.js
+++ pages/options/index.js
@@ -28,17 +28,32 @@ class OptionsPage extends Component {
         A: false,
         S: false,
       },
-      gender: '',
+      gender: 'All',
+      showHours: false,
+      sortByDistance: false,
+      showDropDown: false,
+      showGeoAlert: true
     }
   }
 
   componentDidMount() {
     document.title = 'Link-SF'
+    this.getGeoPermission()
     fetchTaxonomies()
       .then(taxonomies => {
         this.setState({
           taxonomies: taxonomiesWithIcons(taxonomies)
         })
+      })
+  }
+
+  getGeoPermission() {
+    var geoPermission
+    navigator.permissions.query({'name': 'geolocation'})
+      .then( function(permission) {
+        console.log(permission.state)
+        if(permission.state == 'granted')
+          this.setState({ showGeoAlert: false })
       })
   }
 
@@ -68,50 +83,85 @@ class OptionsPage extends Component {
     this.setState({ demographics: newDemographics })
   }
 
+  toggleDropdown() {
+    this.setState({ showDropDown: !this.state.showDropDown })
+  }
+
+  setCurrentGender(g) {
+    this.setState({ gender: g})
+    this.setState({ showDropDown: false })
+  }
+
+  toggleHours() {
+    this.setState({ showHours: !this.state.showHours })
+    if(this.state.showHours) {
+      this.setHours('open')
+      this.setState({ hours: 'open' })
+    }
+    else {
+      this.setHours('all')
+      this.setState({ hours: 'all' })
+    }
+  }
+
+  toggleDistance() {
+    this.setState({ sortByDistance: !this.state.sortByDistance })
+    if(this.state.sortByDistance) {
+      this.setSortBy('distance')
+      this.setState({ sortBy: 'distance' })
+    }
+    else {
+      this.setSortBy('name')
+      this.setState({ sortBy: 'name' })
+    }
+  }
+
   render() {
-    const { taxonomies, sortBy, hours, services, demographics, gender } = this.state
+    const { taxonomies, sortBy, hours, services, demographics, gender, genders, showHours, sortByDistance, showDropDown, showGeoAlert } = this.state
 
     return (
       <Layout>
         <div className={s.page}>
-          <Group>
-            <Title>
-              Sort by
-            </Title>
-            <ButtonGroup>
-              <LeftButton
-                onClick={() => this.setSortBy('distance')}
-                active={sortBy === 'distance'}
-                disabled="true"
-              >Distance
-              </LeftButton>
-              <RightButton
-                onClick={() => this.setSortBy('name')}
-                active={sortBy === 'name'}
-              >Name
-              </RightButton>
-            </ButtonGroup>
-          </Group>
-          <div className={s.alert}>
-            <strong>Distance disabled:</strong> current location unavailable
-          </div>
-          <Group>
-            <Title>
-              Hours
-            </Title>
-            <ButtonGroup>
-              <LeftButton
-                onClick={() => this.setHours('all')}
-                active={hours === 'all'}
-              >All
-              </LeftButton>
-              <RightButton
-                onClick={() => this.setHours('open')}
-                active={hours === 'open'}
-              >Open Now
-              </RightButton>
-            </ButtonGroup>
-          </Group>
+          <ButtonGroup>
+            <Group className={s.buttonContainer}>
+              <ToggleButton
+                label="Open now"
+                enabled={showHours}
+                onClick={() => this.toggleHours()}
+              />
+              <ToggleButton
+                label="Sort by distance"
+                enabled={sortByDistance}
+                onClick={() => this.toggleDistance()}
+                disableButton={'disabled'}
+              />
+            </Group>
+            <div className={`${s.alert} ${showGeoAlert ? s.hide : ''}`}>
+              <strong>Distance disabled:</strong> current location unavailable
+            </div>
+          </ButtonGroup>
+
+          <ButtonGroup>
+            <ToggleButton
+              label="Welcomes: "
+              gender={gender}
+              onClick={() => this.toggleDropdown()}
+              className={s.genderButton}
+              genderLabel={s.genderLabel}
+            />
+            <ul className={`${s.dropDown} ${showDropDown ? s.enabled : ''}`}>
+              <li onClick={() => this.setCurrentGender('All')}>
+                All
+              </li>
+              <li onClick={() => this.setCurrentGender('Men')}>
+                Men
+              </li>
+              <li onClick={() => this.setCurrentGender('Women')}>
+                Women
+              </li>
+            </ul>
+          </ButtonGroup>
+
           <Group>
             <Title>
               Service
@@ -131,68 +181,53 @@ class OptionsPage extends Component {
             <Title>
               Suitable for
             </Title>
-            <ButtonGroup>
-              <LeftButton
+            <ServiceGroup>
+              <DemographicsButton
                 onClick={() => this.toggleDemographic('C')}
                 active={demographics.C}
               >Children
-              </LeftButton>
-              <MiddleButton
+              </DemographicsButton>
+              <DemographicsButton
                 onClick={() => this.toggleDemographic('Y')}
                 active={demographics.Y}
               >Youth
-              </MiddleButton>
-              <MiddleButton
+              </DemographicsButton>
+              <DemographicsButton
                 onClick={() => this.toggleDemographic('A')}
                 active={demographics.A}
               >Adults
-              </MiddleButton>
-              <RightButton
+              </DemographicsButton>
+              <DemographicsButton
                 onClick={() => this.toggleDemographic('S')}
                 active={demographics.S}
               >Seniors
-              </RightButton>
-            </ButtonGroup>
+              </DemographicsButton>
+            </ServiceGroup>
           </Group>
-          <Group>
-            <Title>
-              Welcome
-            </Title>
-            <ButtonGroup>
-              <LeftButton
-                onClick={() => this.setGender('')}
-                active={gender === ''}
-              >All
-              </LeftButton>
-              <MiddleButton
-                onClick={() => this.setGender('M')}
-                active={gender === 'M'}
-              >Men
-              </MiddleButton>
-              <RightButton
-                onClick={() => this.setGender('F')}
-                active={gender === 'F'}
-              >Women
-              </RightButton>
-            </ButtonGroup>
-          </Group>
-          <div>
-            <button className={s.searchButton}>Search</button>
-          </div>
         </div>
       </Layout>
     )
   }
 }
 
+const ToggleButton = (props) => (
+  <button
+    className={`${s.toggleButton} ${props.className} ${props.enabled ? s.enabled : ''}`}
+    onClick={props.onClick}
+    title={props.label}
+  >
+    <span className={props.genderLabel}>{props.label}</span> {props.gender}
+  </button>
+)
+
 const Group = (props) => (
-  <div {...props} className={s.group}>
+  <div {...props} className={props.className}>
     {props.children}
   </div>
 )
 
 const Title = (props) => (
-  <h1 className={s.title} {...props}></h1>
+  <h1 className={s.header} {...props}></h1>
 )
 
 const ButtonGroup = (props) => (
@@ -219,25 +254,15 @@ const filterOut = (props, thingsToExclude) => {
 const ServiceFilter = (props) => (
   <button className={[s.service, props.active ? s.active : ''].join(' ')} {...filterOut(props, ['active', 'category'])}>
     <i className={`${s.categoryIcon} ${props.category.icon}`}></i>
-    {props.category.name}
+    <span className={s.categoryTitle}>{props.category.name}</span>
+    <i className={`${props.active ? icons.iconCheck:icons.iconCheckEmpty} ${s.checkBox}`}></i>
   </button>
 )
 
-const LeftButton = (props) => (
-  <button className={[s.button, s.leftButton, props.active ? s.active : ''].join(' ')} {...filterOut(props, ['active'])}>
-    {props.children}
-  </button>
-)
-
-const MiddleButton = (props) => (
-  <button className={[s.button, s.middleButton, props.active ? s.active : ''].join(' ')} {...filterOut(props, ['active'])}>
-    {props.children}
-  </button>
-)
-
-const RightButton = (props) => (
-  <button className={[s.button, s.rightButton, props.active ? s.active : '' ].join(' ')} {...filterOut(props, ['active'])}>
-    {props.children}
+const DemographicsButton = (props) => (
+  <button className={[s.button, s.demographicsButton, props.active ? s.active : ''].join(' ')} {...filterOut(props, ['active'])}>
+    <span className={s.demographicsTitle}>{props.children}</span>
+    <i className={`${props.active ? icons.iconCheck:icons.iconCheckEmpty} ${s.checkBox}`}></i>
   </button>
 )
 
