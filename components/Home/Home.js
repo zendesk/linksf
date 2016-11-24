45a48fb4abd38e1723bc2dda0470c28633bc414d
Craig Day <cday@zendesk.com>
2016-11-23 17:39:47 -0800

    fix some basic styles on the home page

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -4,9 +4,12 @@ import CategoryList from '../CategoryList'
 import icons from '../../icons/css/icons.css'
 
 const Home = (props) => (
-  <div className={s.taxonomyList}>
-    <p className={s.title}>What service are you looking for?</p>
-    <CategoryList categories={props.categories} />
+  <div>
+    <div className={s.spacer}></div>
+    <div className={s.taxonomyList}>
+      <p className={s.title}>What service are you looking for?</p>
+      <CategoryList categories={props.categories} />
+    </div>
   </div>
 )
 
