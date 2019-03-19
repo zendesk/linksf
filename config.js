704ea84c139d069422bd984c2c9b2a27c2e58eef
Adam Hart <ahart@zendesk.com>
2019-03-19 11:45:57 -0700

    Add back config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+export default {
+  title: '',
+  description: '',
+
+  firebaseAuthDomain: "[PROJECT_ID].firebaseapp.com",
+  firebaseDatabaseUrl: "https://[PROJECT_ID].firebaseio.com",
+  firebaseStorageBucket: "[PROJECT_ID].appspot.com",
+
+  firebaseApiKey: "[FIREBASE_API_KEY]",
+
+  feedbackEmailAddress: ""
+}
